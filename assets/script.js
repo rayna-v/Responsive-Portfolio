d6ea1e0b4710064b50e884fe5265bdac5efeb6e0
Jambi 46.2 <69945554+rayna-v@users.noreply.github.com>
2021-01-31 23:01:00 -0700

    added resume to open for print/download; changed some styles; added gifs and links to repos

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+// const btnDownload = document.getElementById("btn-download")
+// console.log(btnDownload)
+// // btnDownload.setAttribute("src", "./Resume/Rayna_Veuleman_Resume.PDF.pdf")
+
+// btnDownload.addEventListener("click", (event) => {
+//     console.log("clicked")
+//     let imgPath = event.target.getAttribute("src");
+//     let fileName = "rayna-veuleman-resume"
+//     console.log(fileName)
+//     saveAs(imgPath, fileName)
+// });
